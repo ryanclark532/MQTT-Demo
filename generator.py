e2ae0c3937d722ce460851d27456ef55b5dfd93d
Ryan Clark <42229549+ryanclark532@users.noreply.github.com>
2021-09-20 21:27:40 +1000

    removed unused print statment

--- generator.py
+++ generator.py
@@ -7,6 +7,5 @@ generator_client.connect("127.0.0.1", 1883, 60)
 
 while True:
     data = str(randint(0, 100))
-    print(data)
     generator_client.publish(topic, data)
     time.sleep(randint(0, 30))
